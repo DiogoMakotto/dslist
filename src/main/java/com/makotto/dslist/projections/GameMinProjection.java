5240160a4af5484d2e33cc8a7f0905745e69fb03
DiogoMakotto <diogomakotto@gmail.com>
2024-11-16 22:56:53 -0300

    GET findByList

---
+++ src/main/java/com/makotto/dslist/projections/GameMinProjection.java
@@ -0,0 +1,16 @@
+package com.makotto.dslist.projections;
+
+public interface GameMinProjection {
+
+    Long getId();
+
+    String getTitle();
+
+    Integer getYear();
+
+    String getImgUrl();
+
+    String getShortDescription();
+
+    Integer getposition();
+}
--- src/main/java/com/makotto/dslist/service/GameService.java
+++ src/main/java/com/makotto/dslist/service/GameService.java
@@ -3,6 +3,7 @@ package com.makotto.dslist.service;
 import com.makotto.dslist.dto.GameDTO;
 import com.makotto.dslist.dto.GameMinDTO;
 import com.makotto.dslist.entities.Game;
+import com.makotto.dslist.projections.GameMinProjection;
 import com.makotto.dslist.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -25,6 +26,11 @@ public class GameService {
     @Transactional(readOnly = true)
     public List<GameMinDTO> findAll() {
         List<Game> result = gameRepository.findAll();
+        return result.stream().map(x -> new GameMinDTO(x)).toList();
+    }
+    @Transactional(readOnly = true)
+    public List<GameMinDTO> findByList(Long listId) {
+        List<GameMinProjection> result = gameRepository.searchByList(listId);
         return result.stream().map(x -> new GameMinDTO(x)).toList();
     }
 }
